70802d95d0e588e3a77cb27831095ecd4f140904
Trauma <ivanyazcul@gmail.com>
2024-12-07 16:04:58 +0300

    Небольшие правки, изменения кода

--- utils.h
+++ utils.h
@@ -7,4 +7,7 @@ int is_number(const char *str);
 // Выполняет вычисление двух чисел с указанным оператором
 int calculate(int left_operand, char calc_operator, int right_operand);
 
+// Вывод результирующей строки
+void print_result_string(int *results, int res_cnt);
+
 #endif
